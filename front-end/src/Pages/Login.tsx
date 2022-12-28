02a5200826542d7fa5638a377aff306e2bd1baf9
Hugo Souza <hugo.html.com@gmail.com>
2022-12-28 18:57:44 -0300

    Estilização login

--- front-end/src/Pages/Login.tsx
+++ front-end/src/Pages/Login.tsx
@@ -4,12 +4,12 @@ export const Login = () => {
         <div className="min-h-screen min-w-screen flex items-center justify-center bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300">
             <div className="flex flex-col shadow-xl">
                 <div className="py-6 px-14 bg-gradient-to-tr from-orange-500 to-red-600 rounded-tl-2xl rounded-tr-2xl text-center space-y-8">
-                <h2 className="text-white text-xs uppercase">Olá, tudo bem? 😀</h2>
                 <h4 className="text-white text-center font-bold text-xl">
-                    Vamos fazer login agora? 
+                    Login 
                 </h4>
                 </div>
                 <div className="flex flex-col py-6 px-8 space-y-5 bg-white">
+                <h2 className="text-black text-center text-xs uppercase">Olá, tudo bem? 😀</h2>
                 <label>Empresa: 🏭 </label>
                 <input type="text" placeholder="Empresa" className="px-2 py-2 border-2 rounded-md border-gray-200 focus:outline-none focus:border-orange-500 ease-in duration-300" />
                 <label>Senha: 🔒</label>
